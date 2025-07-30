9a5811274a08e35207e5704b6e0804840668b02a
mag1c1an1 <mag1cian@icloud.com>
2025-07-30 18:57:56 +0800

    add uv and xmake
    Signed-off-by: mag1c1an1 <mag1cian@icloud.com>

---
+++ cpp/src/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * SPDX-FileCopyrightText: LakeSoul Contributors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+
+#include <lakesoul/lakesoul_dataset.h>
+
+int main() {
+  printf("xxxx");
+  return 0;
+}
